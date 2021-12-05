b0988840acfec15e0924e047b102d3e3936e1c2f
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-05 18:13:53 +0600

    Add files via upload

---
+++ sum of odd num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,sum=0;
+    for(i=1;i<=100;i=i+2)
+    {
+     sum=sum+i;
+    }
+printf("the sum of 100 odd number is=%d",sum);
+
+
+    getch();
+}
